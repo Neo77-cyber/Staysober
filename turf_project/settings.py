6715c7618ca0954ac9a809865a06e1bbb17a12b4
Neo_codez <holyjazzy1@gmail.com>
2026-04-29 15:47:59 +0100

    Testing for 50 Users

--- turf_project/settings.py
+++ turf_project/settings.py
@@ -250,6 +250,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')       
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='')
 DEFAULT_FROM_EMAIL = 'DearSelf <noreply@dearself.app>'
